89496128efdab2a3a9e2de5126f4401db9ca509c
Ray suh <readingod@naver.com>
2021-06-29 10:27:07 +0900

    chore: 코멘트 추가

--- ProtocolExtension/ProtocolRay/ProtocolRay/ViewController/ViewModelBindableType.swift
+++ ProtocolExtension/ProtocolRay/ProtocolRay/ViewController/ViewModelBindableType.swift
@@ -8,6 +8,7 @@ protocol ViewModelBindableType {
     func bindViewModel()
 }
 
+//Protocol Extension 학습코드
 extension ViewModelBindableType where Self: UIViewController {
     mutating func bind(viewModel: ViewModelType) {
         self.viewModel = viewModel
