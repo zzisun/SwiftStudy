cbd6c896154555079b2436742199cee94e8c5648
Ray suh <readingod@naver.com>
2021-08-26 15:51:22 +0900

    feat: [#12] shape, overlay, shadow 추가

--- Study/SwiftUI/Landmarks/Landmarks/Views/CircleImage.swift
+++ Study/SwiftUI/Landmarks/Landmarks/Views/CircleImage.swift
@@ -3,6 +3,9 @@ import SwiftUI
 struct CircleImage: View {
     var body: some View {
         Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 2))
+            .shadow(radius: 7)
     }
 }
 
