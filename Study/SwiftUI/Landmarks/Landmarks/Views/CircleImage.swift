ae86c12dedb4cdd7e2b741296f2cd4ae9dbf4888
Ray suh <readingod@naver.com>
2021-08-26 15:50:10 +0900

    feat: [#12] circleimage 추가

---
+++ Study/SwiftUI/Landmarks/Landmarks/Views/CircleImage.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
