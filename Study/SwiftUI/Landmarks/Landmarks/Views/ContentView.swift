c5cc9e16729ba2ed6b85ea3cfc2ea65f6e2052e1
Ray suh <readingod@naver.com>
2021-08-26 15:58:09 +0900

    feaet: [#12] ContentView에 mapview && circleImage 추가

--- Study/SwiftUI/Landmarks/Landmarks/Views/ContentView.swift
+++ Study/SwiftUI/Landmarks/Landmarks/Views/ContentView.swift
@@ -2,17 +2,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtule Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                Spacer()
-                Text("California")
+        VStack {
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtule Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
             }
-            .font(.subheadline)
+            .padding()
         }
-        .padding()
     }
 }
 
