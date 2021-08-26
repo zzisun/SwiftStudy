bb785ec47cd193c329da274e1b0f647916b9bc2e
Ray suh <readingod@naver.com>
2021-08-26 15:52:21 +0900

    feat: [#12] MapView 생성

---
+++ Study/SwiftUI/Landmarks/Landmarks/Views/MapView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
