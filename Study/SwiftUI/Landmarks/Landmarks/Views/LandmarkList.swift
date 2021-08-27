ca0498bc5cff08ccf0a272a72d65d10042bbe622
Ray suh <readingod@naver.com>
2021-08-27 13:51:56 +0900

    feat: [#12] Add environmentObject to check the preview

--- Study/SwiftUI/Landmarks/Landmarks/Views/LandmarkList.swift
+++ Study/SwiftUI/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -33,5 +33,6 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList()
+            .environmentObject(ModelData())
     }
 }
