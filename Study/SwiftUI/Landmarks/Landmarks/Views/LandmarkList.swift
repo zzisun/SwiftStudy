bb1acd36168bd3c06e82ea12223526ad63aeff67
Ray suh <readingod@naver.com>
2021-08-27 11:28:41 +0900

    feat: [#12] add a state property

--- Study/SwiftUI/Landmarks/Landmarks/Views/LandmarkList.swift
+++ Study/SwiftUI/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -1,6 +1,14 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationView {
             List(landmarks) { landmark in
@@ -15,10 +23,6 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }
+        LandmarkList()
     }
 }
