5265e5f43a5f0f3b2e71985951eac18a400e2f27
Ray suh <readingod@naver.com>
2021-08-27 13:40:56 +0900

    feat: [#12] Add a Favorite Button

---
+++ Study/SwiftUI/Landmarks/Landmarks/Views/FavoriteButton.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct FavoriteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton()
+    }
+}
