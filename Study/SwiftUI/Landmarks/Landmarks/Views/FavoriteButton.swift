e022664140d8c7231ea9f9b8b4f3c8bc23582de8
Ray suh <readingod@naver.com>
2021-08-27 13:41:48 +0900

    feat: [#12] Add a Binding Property

--- Study/SwiftUI/Landmarks/Landmarks/Views/FavoriteButton.swift
+++ Study/SwiftUI/Landmarks/Landmarks/Views/FavoriteButton.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
@@ -8,6 +10,6 @@ struct FavoriteButton: View {
 
 struct FavoriteButton_Previews: PreviewProvider {
     static var previews: some View {
-        FavoriteButton()
+        FavoriteButton(isSet: .constant(true))
     }
 }
