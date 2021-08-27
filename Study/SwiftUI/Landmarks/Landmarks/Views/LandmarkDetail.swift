9f75ab01e43685c19c940947456207e2e7a55ac3
Ray suh <readingod@naver.com>
2021-08-27 09:42:06 +0900

    feat: [#12] copy contents of the body property from ContentView

--- Study/SwiftUI/Landmarks/Landmarks/Views/LandmarkDetail.swift
+++ Study/SwiftUI/Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -2,7 +2,39 @@ import SwiftUI
 
 struct LandmarkDetail: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundColor(.primary)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+            
+            Spacer()
+        }
+        
     }
 }
 
