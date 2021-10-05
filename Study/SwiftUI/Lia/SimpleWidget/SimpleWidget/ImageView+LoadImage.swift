ce99e143de540c8b309a2f96c7de455a198764c0
Lia <kjk0825@postech.ac.kr>
2021-10-05 17:14:05 +0900

    feat✨: add Kingfisher package & load func

---
+++ Study/SwiftUI/Lia/SimpleWidget/SimpleWidget/ImageView+LoadImage.swift
@@ -0,0 +1,26 @@
+//
+//  ImageView+LoadImage.swift
+//  SimpleWidget
+//
+//  Created by Lia on 2021/10/05.
+//
+
+import UIKit
+import Kingfisher
+
+extension UIImageView {
+    
+    func load(url: String?) {
+        let url = URL(string: url!)
+        self.kf.setImage(with: url) { result in
+            switch result {
+            case .success(_):
+                break
+            case .failure(_):
+                self.image = UIImage(named: "LoadFail.jpg")
+            }
+        }
+    }
+
+}
+
