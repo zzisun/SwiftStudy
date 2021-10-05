1e4bd7d2855295ffd9c8f18b1e5203e981824970
Lia <kjk0825@postech.ac.kr>
2021-10-05 17:08:53 +0900

    refactor:🔨 Widget View & Provider 분리

--- Study/SwiftUI/Lia/SimpleWidget/FirstWidget/FirstWidget.swift
+++ Study/SwiftUI/Lia/SimpleWidget/FirstWidget/FirstWidget.swift
@@ -8,41 +8,36 @@
 import WidgetKit
 import SwiftUI
 
+
+let snapshotEntry = UserWidget(
+    date: Date(),
+    id: 1,
+    email: "Lia@316.com",
+    name: "Lia",
+    profileImage: "Lia"
+)
+
 struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+    func placeholder(in context: Context) -> UserWidget {
+        snapshotEntry
     }
 
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
+    func getSnapshot(in context: Context, completion: @escaping (UserWidget) -> ()) {
+        let entry = snapshotEntry
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
+        var entries: [UserWidget] = []
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
-            entries.append(entry)
+        for index in 0 ..< 5 {
+            entries[index].date = Calendar.current.date(byAdding: .hour, value: index, to: currentDate)!
         }
 
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
-    }
-}
-
-struct SimpleEntry: TimelineEntry {
-    let date: Date
-}
-
-struct FirstWidgetEntryView : View {
-    var entry: Provider.Entry
-
-    var body: some View {
-        Text(entry.date, style: .time)
     }
 }
 
@@ -52,16 +47,9 @@ struct FirstWidget: Widget {
 
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            FirstWidgetEntryView(entry: entry)
+            FirstWidgetEntryView(model: entry)
         }
         .configurationDisplayName("My Widget")
         .description("This is an example widget.")
-    }
-}
-
-struct FirstWidget_Previews: PreviewProvider {
-    static var previews: some View {
-        FirstWidgetEntryView(entry: SimpleEntry(date: Date()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
--- Study/SwiftUI/Lia/SimpleWidget/FirstWidget/UserWidget.swift
+++ Study/SwiftUI/Lia/SimpleWidget/FirstWidget/UserWidget.swift
@@ -8,7 +8,7 @@
 import Foundation
 import WidgetKit
 
-struct User: Decodable, TimelineEntry {
+struct UserWidget: Decodable, TimelineEntry {
     var date = Date()
     let id: Int
     let email: String
