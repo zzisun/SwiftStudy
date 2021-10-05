8af9f1215bc270cd780ebff109a859ae2e9c9499
Lia <kjk0825@postech.ac.kr>
2021-10-05 12:08:06 +0900

    feat✨: initial commit

---
+++ Study/SwiftUI/Lia/SimpleWidget/SimpleWidget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleWidget
+//
+//  Created by Lia on 2021/10/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Study/SwiftUI/Lia/SimpleWidget/SimpleWidget/SimpleWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWidgetApp.swift
+//  SimpleWidget
+//
+//  Created by Lia on 2021/10/05.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
