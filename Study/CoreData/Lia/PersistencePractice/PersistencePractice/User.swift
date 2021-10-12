e1adeba5f573c66123916614296fe23692045ec6
Lia <kjk0825@postech.ac.kr>
2021-10-12 16:01:50 +0900

    feat✨: create model - user

---
+++ Study/CoreData/Lia/PersistencePractice/PersistencePractice/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  PersistencePractice
+//
+//  Created by Lia on 2021/10/12.
+//
+
+import Foundation
+
+struct User {
+    var id: Int
+    var name: String
+    var email: String
+    var profileImage: String
+}
+
+let lia = User(id: 5,
+               name: "Lia",
+               email: "Lia316@p.ac.kr",
+               profileImage: "https://user-images.githubusercontent.com/73650994/136906993-42601a95-a52f-4cea-a559-8e2bf6c661d5.png")
