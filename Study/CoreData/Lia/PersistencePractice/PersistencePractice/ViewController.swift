d6b1fd2a61d5f8a10fd7ef00acb3c61d0f8d1dce
Lia <kjk0825@postech.ac.kr>
2021-10-12 15:44:57 +0900

    feat✨: initial commit

---
+++ Study/CoreData/Lia/PersistencePractice/PersistencePractice/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PersistencePractice
+//
+//  Created by Lia on 2021/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
